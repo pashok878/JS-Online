fb02ef80336428e8ca909243f2d4717f453a09a5
Pavel Martynenkov <pmartynenkov@moysklad.ru>
2018-03-29 23:13:30 +0300

    Lesson 5(part of it)

---
+++ lesson5/script.js
@@ -0,0 +1,26 @@
+var div = document.querySelectorAll('*')
+console.log()
+
+var elem = document.querySelector('*')
+
+var texts = 0, comments = 0
+
+textAndCommentNodes(elem)
+
+console.log("Tags: " + div.length + "\nText nodes: " + texts + "\nComment nodes: " + comments)
+
+
+function textAndCommentNodes(node){
+	var textAndCommNodes = []
+
+	  for (node = node.firstChild; node; node = node.nextSibling) {
+	    if (node.nodeType == 3) {
+	    	texts++
+	    } else if (node.nodeType == 8) {
+	    	comments++
+	    } else {
+	    	//textAndCommNodes = textAndCommNodes.concat(textAndCommentNodes(node));
+	    	textAndCommentNodes(node)
+	    } 
+	  }
+}
